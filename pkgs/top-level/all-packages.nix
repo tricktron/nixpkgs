aa7790706552fc07f70ecf207af17fb7a78864a4
Frederik Rietdijk <fridh@fridh.nl>
2019-10-11 13:58:44 +0200

    python38: test builds using rc1
    Perform a single run to see if there is already any new breakage with a rc.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8922,7 +8922,7 @@ in
   python35Packages = python35.pkgs;
   python36Packages = python36.pkgs;
   python37Packages = recurseIntoAttrs python37.pkgs;
-  python38Packages = python38.pkgs;
+  python38Packages = recurseIntoAttrs python38.pkgs;
   pypyPackages = pypy.pkgs;
   pypy2Packages = pypy2.pkgs;
   pypy27Packages = pypy27.pkgs;
