92690fc6f4c452b26218fc8d1b2ceec4048cb7de
Thibault Gagnaux <tgagnaux@gmail.com>
2022-08-17 15:08:49 +0200

    libplacebo: enable demos

--- pkgs/development/libraries/libplacebo/default.nix
+++ pkgs/development/libraries/libplacebo/default.nix
@@ -13,6 +13,9 @@
 , libGL
 , xorg
 , libunwind
+, ffmpeg_5
+, SDL2
+, SDL2_image
 }:
 
 stdenv.mkDerivation rec {
@@ -23,8 +26,9 @@ stdenv.mkDerivation rec {
     domain = "code.videolan.org";
     owner = "videolan";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "161dp5781s74ca3gglaxlmchx7glyshf0wg43w98pl22n1jcm5qk";
+    fetchSubmodules = true;
+    rev = "1d3ff4d4091a8c91cecdf3f1892ed0d1e1bf01cc";
+    sha256 = "sha256-C5Akp+BLp/QojyhRRQzW83OieNJkmC/ECJKPsAkKdPk=";
   };
 
   nativeBuildInputs = [
@@ -32,27 +36,38 @@ stdenv.mkDerivation rec {
     ninja
     pkg-config
     python3Packages.Mako
+    python3Packages.setuptools
+    glslang # needed at runtime for the demos
   ];
 
   buildInputs = [
     vulkan-headers
     vulkan-loader
     shaderc
-    glslang
     lcms2
     libepoxy
     libGL
     xorg.libX11
     libunwind
+    ffmpeg_5
+    SDL2
+    SDL2_image
   ];
 
   mesonFlags = [
+    "-Dvulkan=enabled"
     "-Dvulkan-registry=${vulkan-headers}/share/vulkan/registry/vk.xml"
-    "-Ddemos=false" # Don't build and install the demo programs
+    "-Dvulkan-link=true"
+    "-Ddebug-abort=true"
+    "-Dglslang=enabled"
+    "-Dopengl=disabled"
+    "-Dshaderc=disabled"
     "-Dd3d11=disabled" # Disable the Direct3D 11 based renderer
   ] ++ lib.optionals stdenv.isDarwin [
     "-Dunwind=disabled" # libplacebo doesn’t build with `darwin.libunwind`
   ];
+
+  LD_LIBRARY_PATH = "${vulkan-loader}/lib"; # needed for the demos at runtime
 
   meta = with lib; {
     description = "Reusable library for GPU-accelerated video/image rendering primitives";
