54c9729cd3931d4406b479c0a4e48de6e48bc845
Thibault Gagnaux <tgagnaux@gmail.com>
2023-05-10 09:12:41 +0200

    testkube: add ld flags and shell completion

--- pkgs/development/tools/testkube/default.nix
+++ pkgs/development/tools/testkube/default.nix
@@ -1,4 +1,4 @@
-{ buildGoModule, fetchFromGitHub, lib, stdenv }:
+{ buildGoModule, fetchFromGitHub, lib, stdenv, installShellFiles }:
 
 buildGoModule rec {
   pname = "testkube";
@@ -9,23 +9,39 @@ buildGoModule rec {
     repo = pname;
     rev = "v${version}";
     sha256 = "sha256-6U9tbn1hp4OEI5ZNcknXBm64nJtZtC9nuIpsHSl1rQw=";
+    # We need the git revision
+    leaveDotGit = true;
+    postFetch = ''
+      git -C $out rev-parse --short HEAD > $out/.git-revision
+      rm -rf $out/.git
+    '';
   };
 
-  subPackages = [ "cmd/kubectl-testkube/main.go" ];
+  nativeBuildInputs = [ installShellFiles ];
 
   vendorSha256 = "sha256-DLh4uKT4/YbA0ZjEZXmryyB5BnlL2XD+8JvHy+Su6pM=";
 
-  ldflags = [ "-s" "-w" ];
+  subPackages = [ "cmd/kubectl-testkube/main.go" ];
+
+  preConfigure = ''
+    ldflags="-s -w -X main.version=${version} \
+    -X main.commit="$(cat .git-revision)" \
+    -X main.date="$(date +%Y-%m-%d)" \
+    -X main.builtBy=nixpkgs"
+  '';
 
   postInstall = ''
     mv $out/bin/main $out/bin/kubectl-testkube
+    installShellCompletion --cmd kubectl-testkube \
+      --bash <($out/bin/kubectl-testkube completion bash) \
+      --fish <($out/bin/kubectl-testkube completion fish) \
+      --zsh <($out/bin/kubectl-testkube completion zsh)
   '';
 
   meta = with lib; {
     description = "Kubernetes-native testing framework for test execution and orchestration";
     homepage = "https://testkube.io/";
     license = licenses.mit;
-    platforms = platforms.linux;
     maintainers = with maintainers; [ mausch ];
   };
 }
