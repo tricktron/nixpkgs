ff0a8144e1087c0db9611f33835d0a02b5dd223c
Thibault Gagnaux <thibault.gagnaux@bit.admin.ch>
2025-05-19 07:25:13 +0200

    chromium fix: try new upstream patchset 4 version
    This is still crashing.

--- pkgs/applications/networking/browsers/chromium/common.nix
+++ pkgs/applications/networking/browsers/chromium/common.nix
@@ -474,7 +474,12 @@ let
         ./patches/widevine-disable-auto-download-allow-bundle.patch
 
         # Use mesa's lib gbm to get a process shareable dma buffer fd
-        ./patches/gbm-wrapper.patch
+        (fetchpatch {
+          name = "use-gbm-to-get-a-process-shareable-dma-buf-fd.patch";
+          url = "https://chromium.googlesource.com/chromium/src/+/d55f8fd5db1d0deb8c398398a2aaf0a364f27eba^!?format=TEXT";
+          decode = "base64 -d";
+          hash = "sha256-yUNneNKozdNdGa3/8DYpwrL2hiYMycVTfi9ctp4OjBE=";
+        })
       ]
       ++ [
         # Required to fix the build with a more recent wayland-protocols version
